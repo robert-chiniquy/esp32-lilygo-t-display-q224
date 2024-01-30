0b6635099efefd0470bf2e5b09506ede55ecaf62
Robert Chiniquy <rchiniquy@yahoo.com>
2024-01-30 14:56:32 -0800

    cargo check passes miraculously, had to get away from esp-idf-svc and juggle/patch a bit to get versions of embedded-hal, display-interface, etc to line up

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,63 @@
-use display_interface::prelude::*;
-use display_interface_spi::*;
-use esp_idf_svc::hal::{gpio::PinDriver, prelude::*, *};
-// use mipidsi::Error;
+use std::error::Error;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    esp_idf_svc::sys::link_patches();
+use display_interface_spi::SPIInterface;
+use embedded_graphics::{image::Image, prelude::*};
+use esp_idf_hal::{
+    delay::Ets,
+    gpio::{AnyIOPin, Gpio5, PinDriver},
+    prelude::*,
+    spi::{config::Config, SpiDeviceDriver, SpiDriver, SpiDriverConfig},
+};
+use mipidsi::Builder;
+use tinybmp::*;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    esp_idf_sys::link_patches();
 
     let peripherals = Peripherals::take().unwrap();
+    let rst = PinDriver::output(peripherals.pins.gpio23)?;
+    let dc = PinDriver::output(peripherals.pins.gpio16)?;
+    let sclk = peripherals.pins.gpio18;
+    let sdo = peripherals.pins.gpio19; // mosi renamed to sdo
 
     let mut backlight = PinDriver::output(peripherals.pins.gpio4)?;
 
-    let dc = PinDriver::output(peripherals.pins.gpio16)?;
-    let mut cs = PinDriver::output(peripherals.pins.gpio5)?;
-    let mut rst = PinDriver::output(peripherals.pins.gpio23)?;
-    let wr = PinDriver::output(peripherals.pins.gpio8)?;
-    let mut rd = PinDriver::output(peripherals.pins.gpio9)?;
+    let _cs = PinDriver::output(peripherals.pins.gpio5)?;
+    let _wr = PinDriver::output(peripherals.pins.gpio8)?;
+    let _rd = PinDriver::output(peripherals.pins.gpio9)?;
+
+    let spi = peripherals.spi2; // or spi3?
+
+    let mut delay = Ets;
+
+    let spi = SpiDriver::new(spi, sclk, sdo, None::<AnyIOPin>, &SpiDriverConfig::new())?;
 
     backlight.set_high()?;
 
-    // set to low to enable display
-    cs.set_low()?;
+    let cs = unsafe { Gpio5::new() };
 
-    // set to high when not in use
-    rd.set_high()?;
+    let spi = SpiDeviceDriver::new(spi, Some(cs), &Config::new())?;
 
-    let spi = peripherals.spi3; // I don't know if I should use SPI2 or 3
-    let sclk = peripherals.pins.gpio18;
-    // --
-    let sdo = peripherals.pins.gpio19; // mosi?
-
-    let di = SPIInterface::new(
-        // Found this example here: https://github.com/ivmarkov/rust-esp32-std-demo/blob/50ab64ed4bf51f02807864458e096c595b94cc1c/src/main.rs#L1045-L1071
-        spi::SpiDeviceDriver::new_single(
-            spi,
-            sclk,
-            sdo,
-            Option::<gpio::AnyIOPin>::None,
-            Option::<gpio::AnyIOPin>::None,
-            // Some(cs),
-            &spi::SpiDriverConfig::new(),
-            &spi::SpiConfig::new(),
-        )?,
-        dc,
-    );
+    //     impl<SPI, DC> WriteOnlyDataCommand for SPIInterface<SPI, DC>
+    // where
+    //     SPI: SpiDevice,
+    //     DC: OutputPin,
+    let di = SPIInterface::new(spi, dc);
 
     // ### WARNING
     // The reset pin needs to be in *high* state in order for the display to operate.
     // If it wasn't provided the user needs to ensure this is the case.
-    rst.set_high().expect(".");
 
-    let mut display = mipidsi::Builder::st7789(di)
-        .init(&mut delay::Ets, Some(rst))
-        .unwrap();
+    let mut display = Builder::st7789_pico1(di)
+        .init(&mut delay, Some(rst))
+        .map_err(|e| format!("{e:?}"))?;
 
-    // // // have tried various ways of drawing anything to screen but LCD always black
-    // // display.clear(RgbColor::RED).unwrap();
-    loop {}
+    let bmp_data = include_bytes!("../ferris.bmp");
+    let bmp = Bmp::from_slice(bmp_data).unwrap();
+
+    Image::new(&bmp, Point::new(0, 0))
+        .draw(&mut display)
+        .map_err(|_| Box::<dyn Error>::from("draw image"))?;
+
+    Ok(())
 }
